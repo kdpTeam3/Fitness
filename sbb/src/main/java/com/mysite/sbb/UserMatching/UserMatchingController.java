3f365f448be1311fb627e6c130cb94a2899cf01e
kitkat <mainist@skkukdp.re.kr>
2024-09-11 14:03:31 +0900

    유저 매칭 시스템 알파타입

--- sbb/src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
+++ sbb/src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
@@ -1,13 +1,17 @@
 package com.mysite.sbb.UserMatching;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserMatchingController {
-
-    @GetMapping("/user-matching")
-    public String showMatchingPage() {
-        return "UserMatchingPage"; // UserMatchingPage.html을 렌더링합니다.
+    @GetMapping("/matching")
+    public String showMatchingPage(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        String formHtml = restTemplate.getForObject("http://localhost:8000/matching-form", String.class);
+        model.addAttribute("matchingForm", formHtml);
+        return "UserMatchingPage";
     }
 }
