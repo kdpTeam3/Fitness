37fad7bc56c583bf4c04fc853b5940ffa4df621b
kitkat <mainist@skkukdp.re.kr>
2024-09-20 15:54:43 +0900

    루틴관련 navbar 링크 수정(make_routine에서 my_routine으로 변경)

--- src/main/java/com/mysite/sbb/workout/bodypart/BodyPartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.sbb.workout.bodypart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BodyPartRepository extends JpaRepository<BodyPart, Long>{
-
-
-}
--- src/main/java/com/mysite/sbb/workout/routine/RoutineRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mysite.sbb.workout.routine;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutineRepository extends JpaRepository<Routine, Long> {
-}
